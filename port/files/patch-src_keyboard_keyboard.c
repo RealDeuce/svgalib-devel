2faa1f6c428d0b83e9fa046816a45d57b1ea7a31
Stephen Hurd <shurd@freebsd.org>
2017-05-14 18:25:16 -0700

    Add translation from syscons scancodes to linux ones.
    This is likely not the right way to do things.

--- port/files/patch-src_keyboard_keyboard.c
+++ port/files/patch-src_keyboard_keyboard.c
@@ -48,3 +48,80 @@
  	        /* if switching vt's, need to clear keystates */
  	        keyboard_clearstate();
  	        /*
+@@ -743,6 +743,76 @@ void keyboard_translatekeys(int mode)
+ 
+ static int kbd_mapkey(int inscancode)
+ {
++	/* Map to Linux codes... */
++	switch(inscancode) {
++		case 0x5c:	/* Print Screen */
++			inscancode = SCANCODE_PRINTSCREEN;
++			break;
++		case 0x68:	/* Pause */
++			inscancode = SCANCODE_BREAK;
++			break;
++		case 0x66:	/* Insert */
++			inscancode = SCANCODE_INSERT;
++			break;
++		case 0x5e:	/* Home */
++			inscancode = SCANCODE_HOME;
++			break;
++		case 0x60:	/* Page Up */
++			inscancode = SCANCODE_PAGEUP;
++			break;
++		case 0x67:	/* Delete */
++			inscancode = SCANCODE_REMOVE;
++			break;
++		case 0x63:	/* End */
++			inscancode = SCANCODE_END;
++			break;
++		case 0x65:	/* Page Down */
++			inscancode = SCANCODE_PAGEDOWN;
++			break;
++		case 0x5f:	/* Up Arrow */
++			inscancode = SCANCODE_CURSORBLOCKUP;
++			break;
++		case 0x64:	/* Down Arrow */
++			inscancode = SCANCODE_CURSORBLOCKDOWN;
++			break;
++		case 0x61:	/* Left Arrow */
++			inscancode = SCANCODE_CURSORBLOCKLEFT;
++			break;
++		case 0x62:	/* Right Arrow */
++			inscancode = SCANCODE_CURSORBLOCKRIGHT;
++			break;
++		case 0x5b:	/* Keypad Divide */
++			inscancode = SCANCODE_KEYPADDIVIDE;
++			break;
++		case 0x59:	/* Keypad Enter */
++			inscancode = SCANCODE_KEYPADENTER;
++			break;
++		case 0x5d:	/* Right Alt (Alt Graph) */
++			inscancode = SCANCODE_RIGHTALT;
++			break;
++		case 0x69:	/* Left Meta */
++			inscancode = SCANCODE_LEFTWIN;
++			break;
++		case 0x6a:	/* Right Meta */
++			inscancode = SCANCODE_RIGHTWIN;
++			break;
++		case 0x6b:	/* Compose */
++			inscancode = SCANCODE_WINMENU;
++			break;
++		/* Yes, I have a Sun keyboard, Just mapping "unused" codes */
++		case 0x6d:	/* Stop */
++			inscancode = 89;
++			break;
++		case 0x6e:	/* Again */
++			inscancode = 90;
++			break;
++		case 0x6f:	/* Copy */
++			inscancode = 91;
++			break;
++		case 0x73:	/* Open */
++			inscancode = 92;
++			break;
++	}
+     if (usekeymap)
+         return keymap[inscancode];
+     else
